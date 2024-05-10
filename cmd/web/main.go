649c8031b0bda6ea89075e7faeb5cde93a6ab417
SCAR FACE <peteradaaku@gmail.com>
2024-05-10 20:28:18 +0100

    few stuffs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"os"
+	"os" // New import
 )
 
 type application struct {
@@ -13,40 +13,32 @@ type application struct {
 }
 
 func main() {
-
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	// Initialize a new instance of our application struct, containing the
-	// dependencies.
-
-	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-	}
-
-	// Swap the route declarations to use the application struct's methods as the
-	// handler functions.
 
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before, and set
+	// the ErrorLog field so that the server now uses the custom errorLog logger in
+	// the event of any problems.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  mux,
 	}
-
 	infoLog.Printf("Starting server on %s", *addr)
+	// Call the ListenAndServe() method on our new http.Server struct.
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
