d97ef9e1a8e0b9466b7050d263f67899dbdcf605
SCAR FACE <peteradaaku@gmail.com>
2024-05-11 16:17:51 +0100

    few stuffs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,7 @@ type application struct {
 }
 
 func main() {
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
 	flag.Parse()
@@ -20,25 +21,21 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux := http.NewServeMux()
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	// Initialize a new http.Server struct. We set the Addr and Handler fields so
-	// that the server uses the same network address and routes as before, and set
-	// the ErrorLog field so that the server now uses the custom errorLog logger in
-	// the event of any problems.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		// Call the new app.routes() method to get the servemux containing our routes.
+		Handler: app.routes(),
 	}
+
 	infoLog.Printf("Starting server on %s", *addr)
-	// Call the ListenAndServe() method on our new http.Server struct.
+
 	err := srv.ListenAndServe()
+
 	errorLog.Fatal(err)
 }
