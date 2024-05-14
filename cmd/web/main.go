7e342a6b90273792d19ab6d187bd11d144458775
Scar Face <peteradaaku@gmail.com>
2024-05-14 06:37:26 +0100

    few changes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,15 @@ import (
 	"net/http"
 	"os"
 
+	"snippetbox.scarface.net/internal/models"
+
 	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -36,6 +39,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
